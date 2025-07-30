2947691fc5ba256982d97471f10b0de87a641ab8
Takumi Yamamoto <yama.moomin08@gmail.com>
2025-07-30 14:35:36 +0900

    add：新規（ファイル）機能追加
    デプロイ用URl追加

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
      allow_origins=[
         "http://localhost:5173",  # ローカル開発用
-        "https://real-estate-app-zeta-eight.vercel.app/"  # ← 実機確認用URL
+        "https://real-estate-app-zeta-eight.vercel.app"  # ← 実機確認用URL
     ],
     allow_credentials=True,
     allow_methods=["*"],
